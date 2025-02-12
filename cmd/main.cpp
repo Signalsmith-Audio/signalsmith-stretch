9a75856509a00dd4667cbb9f37dde02c75dbc652
Geraint Luff <geraint@signalsmith-audio.co.uk>
2025-02-12 17:58:43 +0000

    Add `.setFormantBase()` method (specified relative to sample-rate)

--- cmd/main.cpp
+++ cmd/main.cpp
@@ -16,6 +16,7 @@ int main(int argc, char* argv[]) {
 	double semitones = args.flag<double>("semitones", "pitch-shift amount", 0);
 	double formants = args.flag<double>("formant", "formant-shift amount (semitones)", 0);
 	bool formantComp = args.hasFlag("formant-comp", "formant compensation");
+	double formantBase = args.flag<double>("formant-base", "formant base frequency (Hz, 0=auto)", 0);
 	double tonality = args.flag<double>("tonality", "tonality limit (Hz)", 8000);
 	double time = args.flag<double>("time", "time-stretch factor", 1);
 	bool exactLength = args.hasFlag("exact", "trims the start/end so the output has the correct length");
@@ -35,6 +36,7 @@ int main(int argc, char* argv[]) {
 	stretch.presetDefault(inWav.channels, inWav.sampleRate);
 	stretch.setTransposeSemitones(semitones, tonality/inWav.sampleRate);
 	stretch.setFormantSemitones(formants, formantComp);
+	stretch.setFormantBase(formantBase/inWav.sampleRate);
 
 	// pad the input at the end, since we'll be reading slightly ahead
 	size_t paddedInputLength = inputLength + stretch.inputLatency();
